740085d524a0e586f411e54a12f7d57bdb99f541
Huda Yousif <hjdoski@gmail.com>
2019-10-15 07:20:48 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,6 +1,6 @@
 class Book
-  attr_accessor :author, :page_count, :genre
-  attr_reader :title
+  attr_accessor :author, :page_count
+  attr_reader :title, :genre
   
   GENRES = []
   
@@ -12,4 +12,8 @@ class Book
     puts "Flipping the page...wow, you read fast!"
   end
 
+  def genre=(genre)
+    @genre = genre
+    GENRES << genre 
+  end 
 end
